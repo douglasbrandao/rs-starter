315920642afd90ca78681a68aa635d01ea08b10e
Douglas Brandao <douglasfrb@gmail.com>
2019-06-29 16:49:45 -0400

    template literals

--- curso-es6/main.js
+++ curso-es6/main.js
@@ -1,52 +1,8 @@
-// REST
+const nome = 'Diego';
+const idade = 23;
 
-// objeto
+// TEMPLATE LITERALS utilizamos crase ao invés de aspas
 
-const usuario = {
-    nome: 'Diego',
-    idade: 23,
-    empresa: 'Rocketseat'
-}
+//console.log('Meu nome é ' + nome + ' e tenho ' + idade + ' anos!');
 
-const { nome, ...resto } = usuario
-
-/*console.log(nome);
-console.log(resto);*/
-
-// array
-
-const arr = [1, 2, 3, 4];
-
-const [a, b, ...c] = arr;
-
-/*console.log(a);
-console.log(b);
-console.log(c);*/
-
-// functions
-
-function soma(...params){
-    return params.reduce((total, next) => total + next); // utilizando arrow functions
-}
-
-// console.log(soma(1, 2, 3, 4));
-
-// SPREAD
-
-const arr1 = [1, 2, 3];
-const arr2 = [4, 5, 6];
-
-const arr3 = [...arr1, ...arr2];
-
-// console.log(arr3);
-
-const usuario1 = {
-    nome: 'Diego', 
-    idade: 24
-};
-
-console.log(usuario1);
-
-const usuario2 = { ...usuario1, empresa: 'Rocketseat'};
-
-console.log(usuario2);
+console.log(`Meu nome é ${nome} e tenho ${idade} anos`);
