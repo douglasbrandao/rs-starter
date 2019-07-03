4d3d9ad07123bc1adaadbe2f6e2a4487adf38660
Douglas Brandao <douglasfrb@gmail.com>
2019-07-03 16:05:39 -0400

    Desafio modulo 2 es6

--- curso-es6/src/main.js
+++ curso-es6/src/main.js
@@ -1,1 +1,10 @@
-alert('rssadsadas');
+import ClasseUsuario, { idade, idade as IdadeUsuario } from './functions';
+
+// desafio modulo 2 ex 1.1
+ClasseUsuario.info();
+
+// desafio modulo 2 ex 1.2
+document.getElementById('tela').innerHTML = idade;
+
+// desafio modulo 2 ex 1.3
+console.log(IdadeUsuario);
