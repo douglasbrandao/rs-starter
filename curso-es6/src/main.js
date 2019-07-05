8cbcb2d2fa31fa2fd6f6208ea4ac71eb01348e5c
Douglas Brandao <douglasfrb@gmail.com>
2019-07-04 22:36:19 -0400

    resolucao desafio final modulo 3 es6

--- curso-es6/src/main.js
+++ curso-es6/src/main.js
@@ -1,15 +1,62 @@
-import axios from 'axios';
+// desafio 1 do módulo 3
 
-class Api{
-    static async getUserInfo(username){
-        try{
-            const response = await axios.get(`https://api.github.com/users/${username}`);
-            console.log(response);
-        } catch(err){
-            console.warn('Erro na API');
-        }
-        
+/* const delay = () => new Promise(resolve => setTimeout(resolve, 1000));
+
+async function umPorSegundo(){
+    await delay();
+    console.log('1s');
+    await delay();
+    console.log('2s');
+    await delay();
+    console.log('3s');
+}
+
+umPorSegundo(); */
+
+// desafio 2 do módulo 3
+
+/* import axios from 'axios';
+
+ async function getUserFromGithub(user){
+    try{
+        const response = await axios.get(`https://api.github.com/users/${user}`);
+        console.log(response.data);
+    } catch(erro){
+        console.warn(`O usuário não existe!`);
     }
 }
 
-Api.getUserInfo('douglasbrandao');
+getUserFromGithub('douglasbrandao'); */
+
+// desafio 3 do módulo 3
+
+/* import axios from 'axios';
+
+class Github {
+    static async getRepositories(repo){
+        try{
+            const response = await axios.get(`https://api.github.com/repos/${repo}`);
+            console.log(response.data);
+        } catch(erro){
+            console.warn('Repositório não existe');
+        }
+    }
+}
+
+Github.getRepositories('douglasbrandao/minicurso-django');
+Github.getRepositories('rocketseat/dslkvmskv'); */
+
+// desafio 4 modulo 4
+
+/* import axios from 'axios';
+
+const buscaUsuario = async usuario => {
+    try{
+        const response = await axios.get(`https://api.github.com/users/${usuario}`);
+        console.log(response);
+    } catch(erro){
+        console.warn('Usuário não existe');
+    }
+}
+
+buscaUsuario('douglasbrandao'); */
