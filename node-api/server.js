b57b7cd963703fdba4bd2e07678bfbc5d8740601
Douglas Brandao <douglasfrb@gmail.com>
2019-07-09 18:22:55 -0400

    Conexao com mongodb e node

--- node-api/server.js
+++ node-api/server.js
@@ -1,9 +1,30 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
 
+// iniciando o app
 const app = express();
 
+// iniciando o bd
+
+mongoose.connect(
+    'mongodb://192.168.99.100:27017/nodeapi', 
+    { 
+        useNewUrlParser: true}
+    );
+
+requireDir('./src/models');
+
+const Product = mongoose.model('Product');
+
+// primeira rota
 app.get('/', (req, res) => {
-    res.send('Hello rsrs');
+    Product.create({
+        title: 'React Native', 
+        description: 'Build native apps with React',
+        url: 'http://github.com/facebook/react-native'
+    });
+    return res.send('Hello Rocketseat');
 });
 
 app.listen(3001);
