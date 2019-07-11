a3c76fc32d1d995122ad7f3579b60ebb578bdd6e
Douglas Brandao <douglasfrb@gmail.com>
2019-07-10 23:41:11 -0400

    paginacao e cros

--- node-api/server.js
+++ node-api/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
+const cors = require('cors'); // access control allow origin
 
 // iniciando o app
 const app = express();
 app.use(express.json());
+app.use(cors());
 // iniciando o bd
 
 mongoose.connect(
