31ecc2b5372e4c1da558365c1b5e188ea1d926a2
Douglas Brandao <douglasfrb@gmail.com>
2019-07-12 21:03:05 -0400

    buscando dados na api e listando produtos

---
+++ huntweb/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles.css';
+import Main from './pages/main';
+
+import Header from './components/Header';
+
+const App = () => (
+    <div className="App">
+      <Header />
+      <Main />
+    </div>
+);
+
+export default App;
